6678f8ec3bbd1d26863574cdc61feace9492b6a5
spd99 <89846672+ToniSh69@users.noreply.github.com>
2023-03-10 14:19:51 +0100

    desktop version

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,9 @@ const router = createBrowserRouter(
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </>
   );
 }
--- src/Components/Steps.jsx
+++ src/Components/Steps.jsx
@@ -7,15 +7,31 @@ export default function Steps() {
         <nav>
           <NavLink to="/">
             <button className="step active">1</button>
+            <div className="step-info">
+              <span>STEP 1</span>
+              <p>YOUR INFO</p>
+            </div>
           </NavLink>
           <NavLink to="/select-plan">
             <button className="step">2</button>
+            <div className="step-info">
+              <span>STEP 2</span>
+              <p>SELECT PLAN</p>
+            </div>
           </NavLink>
           <NavLink to="/add-ons">
             <button className="step">3</button>
+            <div className="step-info">
+              <span>STEP 3</span>
+              <p>ADD-ONS</p>
+            </div>
           </NavLink>
           <NavLink to="/finishing-up">
             <button className="step">4</button>
+            <div className="step-info">
+              <span>STEP 4</span>
+              <p>SUMMARY</p>
+            </div>
           </NavLink>
         </nav>
       </header>
